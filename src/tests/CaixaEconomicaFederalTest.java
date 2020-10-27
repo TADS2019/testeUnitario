98c3acd785d2d38c1206730c1d61a8a3b1622274
Guilherme Cordeiro <guilherme.helly@gmail.com>
2020-10-26 21:26:22 -0300

    Finalização

--- src/tests/CaixaEconomicaFederalTest.java
+++ src/tests/CaixaEconomicaFederalTest.java
@@ -1,14 +1,13 @@
 package tests;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Before;
 import org.junit.Test;
-
 import servico.CaixaEconomicaFederal;
 
+import static org.junit.Assert.assertEquals;
+
 /**
- * A classe para testar o cálculo do dígito verificador do código de barras da Caixa Econômica Federal
+ * A classe para testar o c?lculo do d?gito verificador do c?digo de barras da Caixa Econ?mica Federal
  * 
  * @author Filipe Wutzke
  */
@@ -24,9 +23,9 @@ public class CaixaEconomicaFederalTest {
 	}
 	
 	/**
-	 * Testa o calcula do dígito verificador do código de barras
-	 * <ul><b>Dados usados para cálculo:</b>
-	 * <li>Código de barras sem dv: 1049324200000321120055077222133347777777771</li>
+	 * Testa o calcula do d?gito verificador do c?digo de barras
+	 * <ul><b>Dados usados para c?lculo:</b>
+	 * <li>C?digo de barras sem dv: 1049324200000321120055077222133347777777771</li>
 	 * </ul>
 	 * @author Filipe Wutzke
 	 */
@@ -36,15 +35,15 @@ public class CaixaEconomicaFederalTest {
 		digitoCalculado = caixaEconomicaFederal.calcularDigitoVerificadorCodigoDeBarras("1049324200000321120055077222133347777777771");
 		digitoEsperado = "4";
 		
-		assertEquals(String.format("O dígito [%s] retornado é diferente do dígito esperado [%s]", digitoCalculado, digitoEsperado), digitoCalculado,
+		assertEquals(String.format("O d?gito [%s] retornado ? diferente do d?gito esperado [%s]", digitoCalculado, digitoEsperado), digitoCalculado,
 				digitoEsperado);
 	}
 	
 	/**
-	 * Testa o calculo do dígito verificador do campo livre do código de barras</br>
-	 * <ul><b>Dados usados para cálculo:</b>
-	 * <li>Código do Beneficiário - DV: 005507 - 7</li>
-	 * <li>Nosso Número (sem DV): 14/222333777777777</li>
+	 * Testa o calculo do d?gito verificador do campo livre do c?digo de barras</br>
+	 * <ul><b>Dados usados para c?lculo:</b>
+	 * <li>C?digo do Benefici?rio - DV: 005507 - 7</li>
+	 * <li>Nosso N?mero (sem DV): 14/222333777777777</li>
 	 * </ul>
 	 * @author Filipe Wutzke
 	 */
@@ -54,14 +53,14 @@ public class CaixaEconomicaFederalTest {
 		digitoCalculado = caixaEconomicaFederal.calcularDvCampoLivre("005507722213334777777777");
 		digitoEsperado = "1";
 		
-		assertEquals(String.format("O dígito [%s] retornado é diferente do dígito esperado [%s]", digitoCalculado, digitoEsperado), digitoCalculado,
+		assertEquals(String.format("O d?gito [%s] retornado ? diferente do d?gito esperado [%s]", digitoCalculado, digitoEsperado), digitoCalculado,
 				digitoEsperado);
 	}
 	
 	/**
-	 * Testa o calculo do dígito verificador do nosso número</br>
-	 * <ul><b>Dados usados para cálculo:</b>
-	 * <li>Nosso Número usado para cálculo: 14/000000000000019</li>
+	 * Testa o calculo do d?gito verificador do nosso n?mero</br>
+	 * <ul><b>Dados usados para c?lculo:</b>
+	 * <li>Nosso N?mero usado para c?lculo: 14/000000000000019</li>
 	 * </ul>
 	 * @author Filipe Wutzke
 	 */
@@ -71,13 +70,13 @@ public class CaixaEconomicaFederalTest {
 		digitoCalculado = CaixaEconomicaFederal.calcularDvNossoNumero("14000000000000019");
 		digitoEsperado = "7";
 		
-		assertEquals(String.format("O dígito [%s] retornado é diferente do dígito esperado [%s]", digitoCalculado, digitoEsperado), digitoCalculado,
+		assertEquals(String.format("O d?gito [%s] retornado ? diferente do d?gito esperado [%s]", digitoCalculado, digitoEsperado), digitoCalculado,
 				digitoEsperado);
 	}
 	
 	/**
-	 * Teste do DAC (Dígito de Auto Conferência) do primeiro campo da linha digitável.
-	 * <ul><b>Dados usados para cálculo:</b>
+	 * Teste do DAC (D?gito de Auto Confer?ncia) do primeiro campo da linha digit?vel.
+	 * <ul><b>Dados usados para c?lculo:</b>
 	 * <li>Campo 1: 104900550</li>
 	 * </ul>
 	 * @author Filipe Wutzke
@@ -88,7 +87,7 @@ public class CaixaEconomicaFederalTest {
 		int dacCalculado = caixaEconomicaFederal.calcularDACLinhaDigitavel("104900550");
 		int dacEsperado = 5;
 		
-		assertEquals(String.format("O DAC [%s] do primeiro campo retornado é diferente do DAC esperado [%s]", dacCalculado, dacEsperado), dacCalculado,
+		assertEquals(String.format("O DAC [%s] do primeiro campo retornado ? diferente do DAC esperado [%s]", dacCalculado, dacEsperado), dacCalculado,
 				dacEsperado);
 	}
 	
